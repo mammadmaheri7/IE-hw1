b7833c8bce7b912e6ae38704096dffe6d469726f
mohamad maheri <mohamadmaheri@yahoo.com>
2019-10-25 23:49:46 +0330

    final

--- web01/index.js
+++ web01/index.js
@@ -1,13 +1,54 @@
 const express = require('express')
 const whiskers = require('whiskers') 
-
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const inside = require('point-in-polygon')
 const app = express()
 const port = 3000
 
+app.use(bodyParser.json())
+
+
+let polygons;
+if (polygons === undefined) {
+    fs.readFile(`${__dirname}/polygons.json`, (err, data) => {
+        if (err) throw err
+        polygons = JSON.parse(data)
+        
+    })
+}
+
+
+
+
 console.log('--------------------------- NEW APP ')
+console.log(polygons)
 app.use('/' , (req, res, next)=>{
     console.log('Middleware Called!')
     next();
+});
+
+app.get('/gis/testpoint',(req,res) => {
+    let lat = req.query.lat
+    let long = req.query.long
+    let result = { polygons: [] };
+    polygons.features.forEach(element => {
+        if (inside([long, lat], element.geometry.coordinates[0])) {
+            result.polygons.push(element.properties.name)
+        }
+    });
+    res.send(result) ;
+    
+});
+
+app.put('/gis/addpolygon',(req,res)=>{
+    console.log('here')
+    console.log(req.body)
+    polygons.features.push(req.body)
+    fs.writeFile(`${__dirname}/polygons.json`,JSON.stringify(polygons),(err)=>{
+       if(err) throw err
+    })
+    res.sendStatus(200)
 });
 
 app.get('/', (req, res) => {
